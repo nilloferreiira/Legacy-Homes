5b6e9f487bd91fa34c030f44989a582c54ff07fa
Danillo <danillodana@outlook.com>
2024-06-19 13:36:26 -0300

    services and card-service

--- src/components/services/card-service.tsx
+++ src/components/services/card-service.tsx
@@ -1,21 +1,28 @@
-export function CardService() {
+interface CardServiceProps {
+  title: string
+  description: string
+  children: React.ReactNode
+}
+
+export function CardService({
+  title,
+  description,
+  children,
+}: CardServiceProps) {
   return (
     <div
       className={`w-full h-[472px] 
                 bg-shark text-bg 
                 hover:bg-transparent hover:text-shark transition-all 
                 border-2 border-shark/60
-                flex flex-col gap-9 items-center justify-center p-8    
+                flex flex-col gap-9 items-center justify-center p-8 group
     `}
     >
-      <div>Icon</div>
-      <h1 className="">Plumber</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae natus
-        quo nesciunt optio illo blanditiis ad cupiditate pariatur, dolore culpa
-        sapiente magni itaque, doloremque magnam suscipit, totam saepe ut
-        officiis.
-      </p>
+      <div className="group-hover:text-shark transition-colors duration-300">
+        {children}
+      </div>
+      <h1 className="font-semibold text-3xl">{title}</h1>
+      <p className="text-base font-normal">{description}</p>
     </div>
   )
 }
--- src/components/services/grid-cards-services.tsx
+++ src/components/services/grid-cards-services.tsx
@@ -1,15 +1,70 @@
+import { PlumberIconSVG } from '../svg-components/plumber-icon-svg'
 import { CardService } from './card-service'
 
 export function GridCardServices() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto items-center justify-center w-full">
       {/* card  */}
-      <CardService />
-      <CardService />
-      <CardService />
-      <CardService />
-      <CardService />
-      <CardService />
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
+      <CardService
+        title={'Plumber'}
+        description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Saepe reiciendis hic incidunt laboriosam officiis autem,
+                      molestias iste quas sed cumque itaque corporis amet nam. 
+                      Exercitationem consequuntur neque sunt perferendis cupiditate.
+                      `}
+      >
+        <PlumberIconSVG />
+      </CardService>
     </div>
   )
 }
