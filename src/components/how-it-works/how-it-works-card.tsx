29cfaf7a57d256a4298d422c5d52d0b3366c5d1b
Danillo <danillodana@outlook.com>
2024-06-20 18:08:39 -0300

    add how it works completed section

--- src/components/how-it-works/how-it-works-card.tsx
+++ src/components/how-it-works/how-it-works-card.tsx
@@ -1,15 +1,18 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { DescriptionP } from '../generics/description-p'
-import HouseSearching from '../../assets/House searching-pana.png'
-export function HowItWorksCard() {
+
+interface HowItWorksCardProps {
+  title: string
+  description: string
+  image: StaticImageData
+}
+
+export function HowItWorksCard(props: HowItWorksCardProps) {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-5 p-4">
-      <Image src={HouseSearching} alt={''} priority={true} />
-      <h1 className="text-2xl font-extrabold">looking for a tenant</h1>
-      <DescriptionP>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eos,
-        esse excepturi sapiente impedit,s illum velit necessitatibus officia.
-      </DescriptionP>
+      <Image src={props.image} alt={''} priority={true} />
+      <h1 className="text-2xl font-extrabold">{props.title}</h1>
+      <DescriptionP>{props.description}</DescriptionP>
     </div>
   )
 }
--- src/components/how-it-works/how-it-works-grid.tsx
+++ src/components/how-it-works/how-it-works-grid.tsx
@@ -1,20 +1,64 @@
 import { GridCol } from './grid-col'
 import { HowItWorksCard } from './how-it-works-card'
+import HouseLooking from '../../assets/House-searching-pana.png'
+import BusinessMan from '../../assets/Businessman-cuate.png'
+import Marketing from '../../assets/Marketing-cuate.png'
+import InboxCleanUp from '../../assets/Inbox-cleanup-cuate.png'
+import BackToCuate from '../../assets/Back-to-back-cuate.png'
+import BusinessDeal from '../../assets/Business-deal-cuate.png'
 
 export function HowItWorksGrid() {
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center lg:divide-x-2">
       {/* left  */}
       <GridCol>
-        <HowItWorksCard />
-        <HowItWorksCard />
-        <HowItWorksCard />
+        <HowItWorksCard
+          title={'Looking for a tenant?'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={HouseLooking}
+        />
+
+        <HowItWorksCard
+          title={'marketing & promotions'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={Marketing}
+        />
+        <HowItWorksCard
+          title={'tenant selects property'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={BackToCuate}
+        />
       </GridCol>
       {/* right  */}
       <GridCol>
-        <HowItWorksCard />
-        <HowItWorksCard />
-        <HowItWorksCard />
+        <HowItWorksCard
+          title={'A dedicated'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={BusinessMan}
+        />
+        <HowItWorksCard
+          title={'freedom from bogus callers'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={InboxCleanUp}
+        />
+
+        <HowItWorksCard
+          title={'Deal closure'}
+          description={`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Molestiae eius reiciendis laborum quo eveniet sed.
+                        `}
+          image={BusinessDeal}
+        />
       </GridCol>
     </div>
   )
