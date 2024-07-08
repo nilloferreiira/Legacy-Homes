9386f8f203bb5e6b91e0ebe314eb88d7a9593102
Danillo <danillodana@outlook.com>
2024-07-08 14:30:42 -0300

    add clients name to clients testminional card

--- src/components/clients-testimonials/clients-card.tsx
+++ src/components/clients-testimonials/clients-card.tsx
@@ -9,22 +9,22 @@ interface ClientsCardProps {
 
 export function ClientsCard(props: ClientsCardProps) {
   return (
-    <div className="w-full flex flex-col md:flex-row items-start justify-start cursor-pointer bg-white">
+    <div className="w-full lg:h-[285px] flex flex-col md:flex-row items-start justify-start cursor-pointer bg-white">
       <div className="w-full xl:w-4/6">
         <Image src={props.src} alt={''} priority={true} />
       </div>
       {/* criar funcao para mostrar se for o index ativo  */}
       {true && (
-        <div className="w-full p-4 flex flex-col items-start justify-start gap-8">
-          <Quote fill="#1D1D1D" className="size-16" />
+        <div className="w-full p-2 flex flex-col items-start justify-start gap-4">
+          <Quote fill="#1D1D1D" className="size-8" />
           <p className="text-justify tracking-widder font-light w-full text-[#5C5C5C] leading-relaxed">
             {`${props.index.toString()}: Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
             accusantium sunt inventore quisquam ex ducimus quaerat eos
             blanditiis, nemo pariatur facilis reprehenderit! Ratione non
-            consequuntur autem laudantium fugiat cum et.`}
+            consequuntur.`}
           </p>
           <div className="text-left">
-            <h1 className="font-black text-2xl uppercase">maciej witt</h1>
+            <h1 className="font-black text-xl uppercase">maciej witt</h1>
             <h4 className="text-lightGray font-extralight">Singapore</h4>
           </div>
         </div>
